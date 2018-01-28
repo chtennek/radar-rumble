b50ef9fa478447ba2040a01d7fdde55437c5a4ed
David <robotmadeofcheese@yahoo.com>
2018-01-28 14:10:08 -0500

    Updated tile screen Added particles to the rocket

--- Assets/Scripts/Controls - Action/SidescrollerControlManager.cs
+++ Assets/Scripts/Controls - Action/SidescrollerControlManager.cs
@@ -1,88 +1,1 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Collider2D))]
-public class SidescrollerControlManager : MonoBehaviour {
-    public float defaultGravityScale = 2f;
-
-    public float playerHeightAdjust = 0.01f;
-    public float playerWidthAdjust = 0.01f;
-    public float collisionCheckWidth = 0.05f;
-    public LayerMask groundMask;
-
-    private float playerHeight;
-    private float playerWidth;
-
-    private Rigidbody2D rb;
-    private Animator anim;
-    private SpriteRenderer sprite;
-    private PlayerProperties properties;
-    private PeriodicPing periodicPing;
-    private Collider2D col;
-
-    private void Awake() {
-        col = GetComponent<Collider2D>();
-
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>();
-        properties = GetComponent<PlayerProperties>();
-        periodicPing = GetComponent<PeriodicPing>();
-    }
-
-    private void FixedUpdate() {
-        playerWidth = col.bounds.size.x + playerWidthAdjust;
-        playerHeight = col.bounds.size.y + playerHeightAdjust;
-        UpdateAnimation();
-    }
-
-    private void UpdateAnimation() {
-        if (sprite != null) {
-            if (rb.velocity.x > 0) {
-                transform.rotation = Quaternion.identity;
-            }
-            if (rb.velocity.x < 0) {
-                transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
-            }
-        }
-        if (anim != null) {
-            anim.SetFloat("xVelocity", rb.velocity.x);
-            anim.SetFloat("yVelocity", rb.velocity.y);
-            anim.SetFloat("gravityScale", rb.gravityScale);
-            anim.SetBool("isGrounded", IsGrounded());
-            anim.SetBool("isAgainstWall", IsGrounded(Vector2.left) || IsGrounded(Vector2.right));
-        }
-        if (properties != null) {
-            if (rb.velocity.x > 0) {
-                properties.isFacingRight = true;
-            }
-            else if (rb.velocity.x < 0) {
-                properties.isFacingRight = false;
-            }
-            if (periodicPing != null) {
-                periodicPing.enabled = rb.velocity.y > 0;
-            }
-            properties.isGrounded = IsGrounded();
-            properties.isWalking = properties.isGrounded && Mathf.Abs(rb.velocity.x) > 0.05;
-        }
-    }
-
-    public bool IsGrounded() { return IsGrounded(Vector2.down, groundMask); }
-    public bool IsGrounded(Vector2 direction) { return IsGrounded(direction, groundMask); }
-    public bool IsGrounded(Vector2 direction, LayerMask colliderMask) {
-        Vector2 boxCenter = (Vector2)transform.position;
-        Vector2 boxSize;
-        if (Mathf.Abs(direction.y) > Mathf.Abs(direction.x)) {
-            boxCenter += 0.5f * playerHeight * transform.lossyScale.y * direction;
-            boxSize = new Vector2(playerWidth * transform.lossyScale.x - collisionCheckWidth, collisionCheckWidth);
-        }
-        else {
-            boxCenter += 0.5f * playerWidth * transform.lossyScale.x * direction;
-            boxSize = new Vector2(collisionCheckWidth, playerHeight * transform.lossyScale.y - collisionCheckWidth);
-        }
-        bool isGrounded = Physics2D.OverlapBox(boxCenter, boxSize, 0, colliderMask);
-        return isGrounded;
-    }
-}
+﻿using System.Collections;using System.Collections.Generic;using UnityEngine;[RequireComponent(typeof(Rigidbody2D))][RequireComponent(typeof(Collider2D))]public class SidescrollerControlManager : MonoBehaviour {    public float defaultGravityScale = 2f;    public float playerHeightAdjust = 0.01f;    public float playerWidthAdjust = 0.01f;    public float collisionCheckWidth = 0.05f;    public LayerMask groundMask;    private float playerHeight;    private float playerWidth;    private Rigidbody2D rb;    private Animator anim;    private SpriteRenderer sprite;    private PlayerProperties properties;    private PeriodicPing periodicPing;    private Collider2D col;    private void Awake() {        col = GetComponent<Collider2D>();        rb = GetComponent<Rigidbody2D>();        anim = GetComponent<Animator>();        sprite = GetComponent<SpriteRenderer>();        properties = GetComponent<PlayerProperties>();        periodicPing = GetComponent<PeriodicPing>();    }    private void FixedUpdate() {        playerWidth = col.bounds.size.x + playerWidthAdjust;        playerHeight = col.bounds.size.y + playerHeightAdjust;        UpdateAnimation();    }    private void UpdateAnimation() {        if (sprite != null) {            if (rb.velocity.x > 0) {                transform.rotation = Quaternion.identity;            }            if (rb.velocity.x < 0) {                transform.rotation = Quaternion.AngleAxis(180, Vector3.up);            }        }        if (anim != null) {            anim.SetFloat("xVelocity", rb.velocity.x);            anim.SetFloat("yVelocity", rb.velocity.y);            anim.SetFloat("gravityScale", rb.gravityScale);            anim.SetBool("isGrounded", IsGrounded());            anim.SetBool("isAgainstWall", IsGrounded(Vector2.left) || IsGrounded(Vector2.right));        }        if (properties != null) {            if (rb.velocity.x > 0) {                properties.isFacingRight = true;            }            else if (rb.velocity.x < 0) {                properties.isFacingRight = false;            }            if (periodicPing != null) {                periodicPing.enabled = rb.velocity.y > 0;            }            properties.isGrounded = IsGrounded();            properties.isWalking = properties.isGrounded && Mathf.Abs(rb.velocity.x) > 0.05;        }    }    public bool IsGrounded() { return IsGrounded(Vector2.down, groundMask); }    public bool IsGrounded(Vector2 direction) { return IsGrounded(direction, groundMask); }    public bool IsGrounded(Vector2 direction, LayerMask colliderMask) {        Vector2 boxCenter = (Vector2)transform.position;        Vector2 boxSize;        if (Mathf.Abs(direction.y) > Mathf.Abs(direction.x)) {            boxCenter += 0.5f * playerHeight * transform.lossyScale.y * direction;            boxSize = new Vector2(playerWidth * transform.lossyScale.x - collisionCheckWidth, collisionCheckWidth);        }        else {            boxCenter += 0.5f * playerWidth * transform.lossyScale.x * direction;            boxSize = new Vector2(collisionCheckWidth, playerHeight * transform.lossyScale.y - collisionCheckWidth);        }        bool isGrounded = Physics2D.OverlapBox(boxCenter, boxSize, 0, colliderMask);        return isGrounded;    }}
---
+++ Assets/Scripts/Particles/Particle.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Particle : MonoBehaviour {
+    // Variables
+    public float activeDuration = 1.0f;
+    public float collidableDuration = 0.8f;
+	public float fadeOutDuration = 0.4f;
+	// References
+	private BoxCollider2D myBoxCollider2D;
+	private Rigidbody2D myRigidbody2D;
+	private SpriteRenderer mySpriteRenderer;
+	private Transform myTransform;
+	// Properties
+	private float _activeDuration;
+	private Color _fadeOutColor = new Color(1f, 1f, 1f, 0.3f);
+	
+	// ========================= Initialization =========================
+	// Use this for initialization
+	void Start () {}
+
+	public void Initialize(Color color) {
+		// Cache references
+		myBoxCollider2D = GetComponent<BoxCollider2D>();
+		myRigidbody2D = GetComponent<Rigidbody2D>();
+		mySpriteRenderer = GetComponent<SpriteRenderer>();
+		myTransform = this.gameObject.transform;
+        mySpriteRenderer.color = color;
+        this.gameObject.SetActive(false);
+	}
+	
+	// ========================= Update =========================
+	public void Activate (Vector3 position, Vector3 direction) {
+		// Enable the particle
+		_activeDuration = activeDuration;
+		this.gameObject.SetActive(true);
+
+		// Reset particle
+		myBoxCollider2D.enabled = false;
+
+		// Set particles position and direction
+		myTransform.position = position;
+		myTransform.rotation = Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+		myRigidbody2D.AddForce(new Vector3((direction.x + Random.Range(-1.2f, 1.2f)) * -0.5f,
+		                                   (direction.y + Random.Range(-1.2f, 1.2f)) * -0.5f, 0f));
+	}
+
+	void Update() {
+		// If particle is active, slowly fade out
+		if (_activeDuration >= 0) {
+			_activeDuration -= Time.deltaTime;
+
+			if (_activeDuration <= 0) {
+				this.gameObject.SetActive(false);
+			}
+			else if (_activeDuration <= fadeOutDuration) {
+				mySpriteRenderer.color = _fadeOutColor;
+			}
+			else if (_activeDuration <= collidableDuration) {
+				myBoxCollider2D.enabled = true;
+			}
+		}
+	}
+}
